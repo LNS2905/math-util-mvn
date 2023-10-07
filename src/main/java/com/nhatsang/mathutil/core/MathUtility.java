f637294b0ae2b0a585e9e20c779d9218806b4098
LNS2905 <nhatsang2905@gmail.com>
2023-10-07 17:09:49 +0700

    1st upload

---
+++ src/main/java/com/nhatsang/mathutil/core/MathUtility.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nhatsang.mathutil.core;
+
+/**
+ *
+ * @author nhats
+ */
+// đây là bộ thư viện chứa các hàm dùng trong tính toán
+//toán học,clone giống class Math bên JDK
+//Khi gọi là thư viện dùng chung mà ko cần nhớ nhung gì sau khi xử lý
+//--> thư viện chứa đồ STATIC
+public class MathUtility {
+
+    public static final double PI = 3.1415;
+
+    //hàm tính n!=1,2,3,4...n
+    //n<0  n>20 ko tính do 21! tràn kiểu long
+    //ko tính giai thừa âm!!!
+    //0! =1! =1
+    public static long getFactorial(int n) {
+        long result = 1;
+
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("Invalid input: n must be a non-negative integer");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+}
